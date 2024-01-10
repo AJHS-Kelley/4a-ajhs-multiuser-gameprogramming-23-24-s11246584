3e0beb250514ed98c9cf40490cbed00abdcc54e6
s11246584 <s11246584@students.duvalschools.org>
2024-01-10 14:04:34 -0500

    Update ProgramTemplate.cs

--- 01_operators/ProgramTemplate.cs
+++ 01_operators/ProgramTemplate.cs
@@ -1,13 +1,45 @@
 // Takiyah Travis, Program Template, v0.01
 using System;
 
-namespace UPDATEFOREACHPROGRAM
+namespace ReviewProject
 {
-    class UPDATEFOREACHPROGRAM
+    class ReviewProject
     {
         static void Main(string[] args)
         {
            
+           // Declaring and Assigning Variables
+           // Integer  -- -2,147,483,438,648 to 2,147,483,438,648
+           // Integar = 4 Bytes
+           int myInt = 221;
+
+           // Long -- -9,147,483,438,648 to 9,147,483,438,648
+           // Long = 8 Bytes
+           long myLong = 4724759203857920;
+
+           // Sting
+           // 2-bytes PER CHARACTER
+           // Must be in Double QUOTES " "
+           string myString "I love nachos!";
+
+           // Float -- Staores up to 6-7 decimal didgits.
+           // Float = 4 Bytes
+           float myFloat = 66.9;
+
+           // Double -- Store up to 15 decimal digits.
+           // Double = 8 Bytes
+           double myDouble = 6.19476830204792373482D;
+
+           // Boolean -- true / false
+           // Boolean = 1 bit
+           bool myBool = false;
+
+           // Charaters (char)
+           // Single character, in SINGLE QUOTES ' '
+           // 2-bytes
+           char myChar = 'C';
+           
+
 
         }
     
